78019a83571236ec162fb8ccc9b8d16a18ff1776
Divy <divyparekh1810@gmail.com>
2023-12-30 02:38:25 +0530

    Connected to mongo atlas

---
+++ src/db/index.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+
+    console.log(`DB connected ${connectionInstance.connection.host}`);
+  } catch (error) {
+    console.error("Error in DB connection ", error);
+    process.exit(1);
+  }
+};
+
+
+export default connectDB;
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.error("Something went wrong in DB connection ", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on PORT : ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("Error in DB connection ", error);
+  }
+})();
+*/
