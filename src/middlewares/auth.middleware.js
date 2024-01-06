5b393016ebfc5583597b937e655947841a3c79c9
Divy <divyparekh1810@gmail.com>
2024-01-06 22:26:08 +0530

    add refresh-token endpoint

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -7,20 +7,16 @@ export const verifyJwt = asyncHandler(async (req, _, next) => {
     const token =
       req.cookies?.accessToken ||
       req.header("Authorization")?.replace("Bearer ", "");
-
     if (!token) {
       throw new ApiError(401, "Unauthorized Request");
     }
-
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-
     const user = await User.findById(decodedToken._id).select(
       "-password -refreshToken"
     );
     if (!user) {
       throw new ApiError(401, "Invalid access token");
     }
-
     req.user = user;
     next();
   } catch (error) {
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
   loginUser,
   logoutUser,
+  refreshAccessToken,
   registerUser,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
@@ -23,9 +24,9 @@ router.route("/register").post(
   registerUser
 );
 router.route("/login").post(loginUser);
-router.route("/logout").post(logoutUser);
 
 //secure routes
 router.route("/logout").post(verifyJwt, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
 
 export default router;
